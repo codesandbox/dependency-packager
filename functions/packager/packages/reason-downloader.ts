0660334ec72736e6fe8263409453ff7131d178c4
Ives van Hoorne <ives.v.h@gmail.com>
2020-12-04 11:50:40 +0100

    Fix reason downloader

--- functions/packager/packages/reason-downloader.ts
+++ functions/packager/packages/reason-downloader.ts
@@ -17,37 +17,40 @@ export function isReason(packageName: string, rootPath: string) {
   return fs.existsSync(bsConfigPath);
 }
 
+type DirSpec = {
+  dir: string;
+  type?: "src" | "dev";
+  subdirs?: boolean | Array<string | DirSpec>;
+};
+
 export async function getReasonFiles(
   rootPath: string,
   packageInfos: { [dep: string]: IPackage },
 ): Promise<IFileData> {
   const nModulesPath = join(rootPath, "node_modules");
   const reasonDependencies = Object.keys(packageInfos)
-    .map(x => packageInfos[x].name)
-    .filter(x => isReason(x, rootPath));
+    .map((x) => packageInfos[x].name)
+    .filter((x) => isReason(x, rootPath));
 
   const files: IFileData = {};
 
   await Promise.all(
-    reasonDependencies.map(async packageName => {
+    reasonDependencies.map(async (packageName) => {
       const packagePath = join(rootPath, "node_modules", packageName);
       const bsConfigPath = join(packagePath, "bsconfig.json");
 
       const bsConfig = await fs
         .readFile(bsConfigPath)
-        .then(data => JSON5.parse(data.toString()));
+        .then((data) => JSON5.parse(data.toString()));
 
-      const sources: Array<
-        | string
-        | { dir: string; type?: "src" | "dev"; subdirs?: string[] | boolean }
-      > =
+      const sources: Array<string | DirSpec> =
         typeof bsConfig.sources === "string"
           ? [bsConfig.sources]
           : bsConfig.sources;
 
       const sourcePaths: Array<string | string[]> = (
         await Promise.all(
-          sources.map(async srcSpec => {
+          sources.map(async (srcSpec) => {
             if (typeof srcSpec === "string") {
               return join(packagePath, srcSpec);
             }
@@ -57,12 +60,18 @@ export async function getReasonFiles(
                 return join(packagePath, srcSpec.dir);
               }
 
-              if (Array.isArray(srcSpec.subdirs)) {
-                return srcSpec.subdirs.map(subdir => join(packagePath, subdir));
+              if (srcSpec.subdirs && Array.isArray(srcSpec.subdirs)) {
+                return srcSpec.subdirs.map((subdir) => {
+                  if (typeof subdir === "string") {
+                    return join(packagePath, srcSpec.dir, subdir);
+                  }
+
+                  return join(packagePath, srcSpec.dir, subdir.dir);
+                });
               } else {
                 // Read all subdirs
-                return recursiveReaddir(packagePath).then(f =>
-                  f.filter(p => fs.lstatSync(p).isDirectory()),
+                return recursiveReaddir(packagePath).then((f) =>
+                  f.filter((p) => fs.lstatSync(p).isDirectory()),
                 );
               }
             } else {
@@ -75,14 +84,14 @@ export async function getReasonFiles(
       const flattenedSources = flatten(sourcePaths);
 
       return Promise.all(
-        flattenedSources.map(async directory => {
+        flattenedSources.map(async (directory) => {
           const reFiles = (await fs.readdir(directory))
-            .map(x => join(directory, x))
-            .filter(x => fs.lstatSync(x).isFile())
-            .filter(x => /\.rei?$/.test(x) || /\.mli?$/.test(x));
+            .map((x) => join(directory, x))
+            .filter((x) => fs.lstatSync(x).isFile())
+            .filter((x) => /\.rei?$/.test(x) || /\.mli?$/.test(x));
 
           return Promise.all(
-            reFiles.map(async filePath => {
+            reFiles.map(async (filePath) => {
               const fileContents = await fs.readFile(filePath);
               files[filePath] = {
                 content: fileContents.toString(),
